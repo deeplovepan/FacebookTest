dc9f96cf59f36280820174a32252d695546b292b
Peter Pan <deeplovepan@gmail.com>
2013-07-24 15:24:28 +0800

    Initial Commit

---
+++ FacebookTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FacebookTest
+//
+//  Created by Pan Peter on 13/7/24.
+//  Copyright (c) 2013年 Pan Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FacebookTestTests/FacebookTestTests.m
@@ -0,0 +1,36 @@
+//
+//  FacebookTestTests.m
+//  FacebookTestTests
+//
+//  Created by Pan Peter on 13/7/24.
+//  Copyright (c) 2013年 Pan Peter. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FacebookTestTests : XCTestCase
+
+@end
+
+@implementation FacebookTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
